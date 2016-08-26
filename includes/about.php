3537a5e4668539d260a346a510e4c67ec0026989
xtr4nge <xtr4nge@gmail.com>
2016-08-26 17:02:09 +0100

    giskismet install has been fixed

--- includes/about.php
+++ includes/about.php
@@ -3,3 +3,11 @@
 kismet identifies networks by passively collecting packets and detecting standard named networks, detecting (and given time, decloaking) hidden networks, and inferring the presence of nonbeaconing networks via data traffic. kismet supports logging to the wtapfile packet format (readable by tcpdump and ethereal) and saves detected network information as plaintext, CSV, and XML.  kismet is capable of using any GPS supported by gpsd and logs and plots network data.
 <br><br>
 kismet is divided into three basic programs, kismet_server kismet_client and gpsmap
+<br><br>
+If you are using a USB GPS, edit <b>/etc/default/gpsd</b>
+<br><br>
+<div style="font-family: courier, monospace;">
+<b>
+DEVICES="/dev/ttyUSB0"
+</b>
+</div>
